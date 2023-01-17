9a2d078260cea4a7a6ac0afc6cbfe65ae6102750
It Hours Solutions <It Hours Solutions@DESKTOP-LARMGGP>
2023-01-17 11:08:47 +0530

    Add project files.

---
+++ ReadOnly_And_Constant_Variable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnly_And_Constant_Variable
+{
+    public class ConstantVariable
+    {
+        public const int x = 100;
+        public readonly int y;
+        ConstantVariable(int xyz)
+        {
+            y = xyz;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(x);
+            ConstantVariable objVar = new ConstantVariable(20);
+            Console.WriteLine(objVar.y);
+            Console.ReadKey();
+        }
+
+    }
+}
